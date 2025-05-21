794477fd5ce81e27d82178d8f0b6b6332def740a
Takagi Tasuku <tasuren@outlook.jp>
2025-05-21 14:46:13 +0900

    Make `event_loop` and `window_observer` modules public

--- src/platform_impl/macos/mod.rs
+++ src/platform_impl/macos/mod.rs
@@ -5,9 +5,9 @@ pub mod ax_function;
 pub mod ax_observer;
 pub mod error_helper;
 pub mod event_helper;
-mod event_loop;
+pub mod event_loop;
 pub mod window;
-mod window_observer;
+pub mod window_observer;
 
 pub use accessibility_sys::AXError as OSError;
 pub use window_observer::MacOSWindowObserver;
