5069147a669d5eab821e618a0cd8dfd2b8ad241d
Takagi Tasuku <tasuren_0618@yahoo.co.jp>
2025-10-27 14:35:48 +0900

    fix: compile error in examples on Windows

--- examples/macos_permission.rs
+++ examples/macos_permission.rs
@@ -1,7 +1,3 @@
-use window_observer::platform_impl::macos::binding_ax_function::{
-    ax_is_process_trusted, ax_is_process_trusted_with_options,
-};
-
 #[cfg(not(target_os = "macos"))]
 fn main() {
     panic!("This example only supports macOS.");
@@ -9,6 +5,10 @@ fn main() {
 
 #[cfg(target_os = "macos")]
 fn main() {
+    use window_observer::platform_impl::macos::binding_ax_function::{
+        ax_is_process_trusted, ax_is_process_trusted_with_options,
+    };
+
     println!("Is process trusted: {}", ax_is_process_trusted());
 
     println!("Prompt if process is not trusted");
